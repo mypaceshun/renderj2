94fa81000a63099c4ddff5aa9c4cfa22e8a13f36
Shun Kawai <shun@osstech.co.jp>
2018-12-13 17:37:16 +0900

    [test]used main function

--- pyrender/pyrender.py
+++ pyrender/pyrender.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
 import click
+import yaml
 
 
 @click.command()
 @click.option('-v', '--varsfile',type=click.File('r'), multiple=True,
               help='vars file path for jinja2')
-@click.argument('template', type=click.File('r'),
-                help='retemplate file path for jinja2')
+@click.argument('template', type=click.File('r'))
 def cmd(template, varsfile):
-    print(template)
-    print(varsfile)
+
+    print(template.name)
+    for line in template:
+        print(line)
+
+    for var in varsfile:
+        print(var.name)
+        data = yaml.load(var)
+        print(data)
 
 def main():
     cmd()
--- test/command_test.sh
+++ test/command_test.sh
@@ -8,6 +8,24 @@ errcount=0
 successcount=0
 TEELOG="tee -a ${LOG}"
 
+main() {
+  loginit
+
+  tmpfile=test/testtemplate.j2
+  varfile=test/testvars.yml
+  varfile2=test/testvars2.yml
+  cmdexec "${CMD} ${tmpfile}"
+  failcmdexec "${CMD} nofile"
+  cmdexec "${CMD} --help"
+  cmdexec "${CMD} --varsfile ${varfile} ${tmpfile}"
+  failcmdexec "${CMD} --varsfile nofile ${tmpfile}"
+  cmdexec "${CMD} --varsfile ${varfile} --varsfile ${varfile2} ${tmpfile}"
+  failcmdexec "${CMD} --varsfile ${varfile} --varsfile nofile ${tmpfile}"
+
+  echo "success   : ${successcount}"
+  echo "error     : ${errcount}"
+}
+
 loginit() {
   rm ${LOG}
   echo "********************">>${LOG}
@@ -32,16 +50,20 @@ cmdexec() {
   echo "" | ${TEELOG}
 }
 
+failcmdexec() {
+  CMDSTR=$1
 
-loginit
+  echo -n "[${CMDSTR}]....." | ${TEELOG}
 
-tmpfile=testtemplate.j2
-varfile=testvars.yml
-varfile2=testvars22.yml
-cmdexec "${CMD} ${tmpfile}"
-cmdexec "${CMD} --help"
-cmdexec "${CMD} --varsfile ${varfile} ${tmpfile}"
-cmdexec "${CMD} --varsfile ${varfile} --varsfile ${varfile2} ${tmpfile}"
+  ${CMDSTR} >> ${LOG} 2>&1
+  if [ $? -eq 0 ]; then
+    echo "error" | ${TEELOG}
+    let errcount++
+  else
+    echo "ok" | ${TEELOG}
+    let successcount++
+  fi
+  echo "" | ${TEELOG}
+}
 
-echo "success   : ${successcount}"
-echo "error     : ${errcount}"
+main
