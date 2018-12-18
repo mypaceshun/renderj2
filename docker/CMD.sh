366a1a2a0ebc826cfad7d6ef332a7034894ec0ca
Shun Kawai <shun@osstech.co.jp>
2018-12-18 17:29:15 +0900

    ビルドするディレクトリを変更
    %{_builddir}は/usr/src/debugより長い文字列でないと
    debuginfoの生成に失敗する。

--- docker/CMD.sh
+++ docker/CMD.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-ls -alhd /rpm/**
-cd /rpm
-rpmbuild --define '_topdir /rpm' -bb SPECS/*.spec
+cd /home/rpmbuilder/rpm
+rpmbuild --define '_topdir /home/rpmbuilder/rpm' -ba SPECS/*.spec
